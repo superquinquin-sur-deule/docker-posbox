cc8cbfb6e0caf5b073dc7c8e82e71df307430238
Florent Jardin <florent@jardin.im>
2026-05-10 12:03:00 +0200

    feat: initialize database on first run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 set -e
+cd /home/odoo/odoo
 
-cat > /home/odoo/odoo/odoo.conf << EOF
+if [ ! -f odoo.conf ]; then
+    python odoo.py \
+        --init=base --without-demo=all --stop-after-init \
+        --database=odoo \
+        --db_host=${POSTGRES_HOST:-db} \
+        --db_user=${POSTGRES_USER:-odoo} \
+        --db_password=${POSTGRES_PASSWORD:-odoo}
+fi
+
+cat > odoo.conf << EOF
 [options]
 db_host = ${POSTGRES_HOST:-db}
 db_user = ${POSTGRES_USER:-odoo}
@@ -10,4 +20,4 @@ db_password = ${POSTGRES_PASSWORD:-odoo}
 server_wide_modules = ${ODOO_MODULES:-hw_proxy,hw_escpos,hw_telium_payment_terminal}
 EOF
 
-exec python /home/odoo/odoo/odoo.py -c /home/odoo/odoo/odoo.conf
+exec python odoo.py --config=odoo.conf
